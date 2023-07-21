347c9f7ffd0d6a73fe04fc31bc068e18a58175a5
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2023-07-21 21:24:01 +0200

    Fix luacheck globals in AddonConfiguration

--- gui/P_AddonConfiguration.lua
+++ gui/P_AddonConfiguration.lua
@@ -23,7 +23,7 @@
   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]--
 
--- luacheck: globals CreateFrame UIParent InterfaceOptions_AddCategory InterfaceOptionsFrame_OpenToCategory
+-- luacheck: globals CreateFrame UIParent Settings
 
 local mod = rgp
 local me = {}
