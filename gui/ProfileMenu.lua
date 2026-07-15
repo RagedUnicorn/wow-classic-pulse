cad4bd9056ef9751eea557717d5fb43396adf7d5
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2026-07-15 12:42:29 +0200

    feat: Clear profileEditBox after saving profile

--- gui/ProfileMenu.lua
+++ gui/ProfileMenu.lua
@@ -514,6 +514,7 @@ FinishImport = function(name, envelope)
 
   mod.profile.SaveProfile(name, envelope.payload)
   me.selectedProfile = name
+  profileEditBox:SetText("")
   RefreshList()
   mod.logger.PrintUserMessage(string.format(rgp.L["profile_import_success"], name))
 end
